ffd3b695195ad78093243870ebc7f318c76a87e1
Shivanagouda S A <shivu.a.1945@gmail.com>
2023-01-01 13:03:49 +0530

    Adding models

---
+++ src/main/java/com/shivu/yifyapp/Models/MovieDetails/Movie.java
@@ -0,0 +1,12 @@
+package com.shivu.yifyapp.Models.MovieDetails;
+
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@NoArgsConstructor
+public class Movie {
+
+    public ExtendedMovieDetails movie;
+    
+}
